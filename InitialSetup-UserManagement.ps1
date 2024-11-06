1fdc96f4db6e5fdceb60e043ce02ec4f4172297a
mariopet <121584492+blueSubzero@users.noreply.github.com>
2024-11-06 23:48:59 +0100

    Add files via upload

---
+++ InitialSetup-UserManagement.ps1
@@ -0,0 +1,8 @@
+﻿Get-LocalUser
+Get-LocalUser | Select-Object *
+Get-LocalGroup
+Get-LocalGroupMember -Group Administrators
+Get-LocalGroupMember -Group Users
+Get-LocalGroupMember -Group Administrators -Member mario | Add-LocalGroupMember -Group users -Verbose
+#Remove-LocalGroupMember -Group Administrators -Member mario
+#Disable-LocalUser -Name Administrator -Verbose
